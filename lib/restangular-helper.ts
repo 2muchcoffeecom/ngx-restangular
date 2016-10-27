a0c2d2a6f10b221587c8a206d06ccaa0258ce8e0
Dmitriy Melnichenko <tairezzzz@gmail.com>
2016-10-27 18:53:51 +0300

    update

---
+++ lib/restangular-helper.ts
@@ -0,0 +1,38 @@
+import {URLSearchParams, Headers, RequestOptions, RequestMethod} from "@angular/http";
+export class RestangularHelper {
+  
+  static createRequestOptions(options) {
+    let requestQueryParams = RestangularHelper.createRequestQueryParams(options.queryParams);
+    let requestHeaders = RestangularHelper.createRequestHeaders(options.headers);
+    let methodName = options.method.charAt(0).toUpperCase() + options.method.substr(1).toLowerCase();
+    
+    let requestOptions = new RequestOptions({
+      method: RequestMethod[methodName],
+      headers: requestHeaders,
+      search: requestQueryParams,
+      url: options.url,
+      body: options.data,
+    });
+
+    return requestOptions;
+  }
+  
+  static createRequestQueryParams(queryParams) {
+    let requestQueryParams = Object.assign({}, queryParams);
+    let search: URLSearchParams = new URLSearchParams();
+    
+    for (let key in requestQueryParams) {
+      let value: any = requestQueryParams[key];
+      if (typeof value === 'object') {
+        value = JSON.stringify(value);
+      }
+      search.append(key, value);
+    }
+    
+    return search;
+  }
+  
+  static createRequestHeaders(headers) {
+    return new Headers(Object.assign({}, headers));
+  }
+}
