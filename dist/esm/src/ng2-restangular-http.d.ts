0a3d8151d697b4776393596125d9ae12148ce120
Logvin Oleg <logvinoleg89@gmail.com>
2016-11-14 12:56:03 +0200

    update gitignore

--- dist/esm/src/ng2-restangular-http.d.ts
+++
@@ -1,8 +0,0 @@
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs';
-export declare class RestangularHttp {
-    http: Http;
-    constructor(http: Http);
-    createRequest(options: any): Observable<any>;
-    request(request: any): Observable<any>;
-}
