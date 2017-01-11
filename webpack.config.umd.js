03a23d34521db53201d587e344351695db4f4ac0
Tamas Csaba <tamcsaba@gmail.com>
2017-01-11 10:35:43 +0100

    chore(bundle): add webpack-node-externals with the aim of not bundle external module

--- webpack.config.umd.js
+++ webpack.config.umd.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
   entry: './src/index.ts',
   output: {
@@ -6,74 +10,7 @@ module.exports = {
     libraryTarget: 'umd',
     library: 'ng2-restangular'
   },
-  externals: {
-    '@angular/core': {
-      root: ['ng', 'core'],
-      commonjs: '@angular/core',
-      commonjs2: '@angular/core',
-      amd: '@angular/core'
-    },
-    '@angular/common': {
-      root: ['ng', 'common'],
-      commonjs: '@angular/common',
-      commonjs2: '@angular/common',
-      amd: '@angular/common'
-    },
-    'rxjs/Subject': {
-      root: ['rx', 'Subject'],
-      commonjs: 'rxjs/Subject',
-      commonjs2: 'rxjs/Subject',
-      amd: 'rxjs/Subject'
-    },
-    'rxjs/Observable': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/Observable',
-      commonjs2: 'rxjs/Observable',
-      amd: 'rxjs/Observable'
-    },
-    'rxjs/observable/merge': {
-      root: ['rx', 'Observable', 'merge'],
-      commonjs: 'rxjs/observable/merge',
-      commonjs2: 'rxjs/observable/merge',
-      amd: 'rxjs/observable/merge'
-    },
-    'rxjs/add/operator/map': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/map',
-      commonjs2: 'rxjs/add/operator/map',
-      amd: 'rxjs/add/operator/map'
-    },
-    'rxjs/add/operator/mergeMap': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/mergeMap',
-      commonjs2: 'rxjs/add/operator/mergeMap',
-      amd: 'rxjs/add/operator/mergeMap'
-    },
-    'rxjs/add/operator/takeUntil': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/takeUntil',
-      commonjs2: 'rxjs/add/operator/takeUntil',
-      amd: 'rxjs/add/operator/takeUntil'
-    },
-    'rxjs/add/operator/filter': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/filter',
-      commonjs2: 'rxjs/add/operator/filter',
-      amd: 'rxjs/add/operator/filter'
-    },
-    'rxjs/add/operator/pairwise': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/pairwise',
-      commonjs2: 'rxjs/add/operator/pairwise',
-      amd: 'rxjs/add/operator/pairwise'
-    },
-    'rxjs/add/operator/take': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/take',
-      commonjs2: 'rxjs/add/operator/take',
-      amd: 'rxjs/add/operator/take'
-    }
-  },
+  externals: [nodeExternals()],
   devtool: 'source-map',
   module: {
     preLoaders: [{
