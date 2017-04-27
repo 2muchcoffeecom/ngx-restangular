fef8d19568daae2b5a2ba1b6705262bcbb712ca4
Logvin Oleg <logvinoleg89@gmail.com>
2017-04-27 16:12:31 +0300

    Revert "fix import rxjs"
    This reverts commit 478b9fb7cb77ece68bb059ce1f3633db5f850f1d.

--- src/ngx-restangular-http.ts
+++ src/ngx-restangular-http.ts
@@ -1,10 +1,6 @@
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-
 import {Injectable} from '@angular/core';
 import {Http, Request} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 
 import {RestangularHelper} from './ngx-restangular-helper';
 
