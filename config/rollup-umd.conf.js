42699dc57282a9244d16abfa5991bcb418ae9387
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-03-23 18:11:13 +0200

    build(config): update umd config
    change globals to output.globals cause of rename in rollup

--- config/rollup-umd.conf.js
+++ config/rollup-umd.conf.js
@@ -3,12 +3,12 @@ export default {
   output: {
     file: 'dist/bundles/ngx-restangular.umd.js',
     format: 'umd',
-    name: 'ngxRestangular'
-  },
-  globals: {
-    // Angular dependencies
-    '@angular/core': 'ng.core',
-    '@angular/common': 'ng.common',
-    '@angular/common/http': 'ng.common.http',
+    name: 'ngxRestangular',
+    globals: {
+      // Angular dependencies
+      '@angular/core': 'ng.core',
+      '@angular/common': 'ng.common',
+      '@angular/common/http': 'ng.common.http',
+    },
   }
 };
