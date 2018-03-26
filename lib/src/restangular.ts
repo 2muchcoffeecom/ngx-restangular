84ed8029280b229fca78ee4152c6f39a4952599f
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-03-26 19:09:59 +0300

    feat(lib): add restangular token for providing

---
+++ lib/src/restangular.ts
@@ -0,0 +1,34 @@
+import { RestangularBuilder } from './builder';
+import { RestangularClient } from './client/client';
+import { RestangularHandler } from './handler';
+import { Injectable } from '@angular/core';
+
+export abstract class Restangular {
+
+  abstract one(id: string | number): RestangularClient;
+  abstract one(route: string, id?: string | number): RestangularClient;
+
+  abstract all(route: string): RestangularClient;
+}
+
+@Injectable()
+export class InitialRestangular {
+
+  constructor(private handler: RestangularHandler) {
+  }
+
+  one(routeOrId, id?) {
+    let route = routeOrId;
+    if (typeof id === 'undefined') {
+      id = routeOrId;
+      route = undefined;
+    }
+    const builder = new RestangularBuilder({id, route, isCollection: false});
+    return new RestangularClient(builder, this.handler);
+  }
+
+  all(route) {
+    const builder = new RestangularBuilder({route, isCollection: true});
+    return new RestangularClient(builder, this.handler);
+  }
+}
