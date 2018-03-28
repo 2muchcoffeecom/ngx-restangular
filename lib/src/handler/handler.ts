e90b2e68c4a9081733ffe33312286bd87bdd512a
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-03-28 11:08:22 +0300

    feat(lib:handler): split handler
    split into BaseHandler & Handler

--- lib/src/handler/handler.ts
+++ lib/src/handler/handler.ts
@@ -4,8 +4,12 @@ import { Observable } from 'rxjs/Observable';
 
 import { RestangularRequest } from '../backend';
 
-export abstract class RestangularHandler {
+export abstract class RestangularBaseHandler {
   abstract handle<T>(req: RestangularRequest<T>): Observable<HttpEvent<T>>;
+
+}
+
+export abstract class RestangularHandler extends RestangularBaseHandler {
 
   abstract withConfig(options: any): RestangularHandler;
 
--- lib/src/handler/intercepting-handler.ts
+++ lib/src/handler/intercepting-handler.ts
@@ -3,11 +3,13 @@ import { HttpResponse } from '@angular/common/http';
 
 import { filter } from 'rxjs/operators/filter';
 
-import { RestangularHandler } from './handler';
+import { RestangularBaseHandler } from './handler';
 import { RestangularBackend, RestangularRequest } from '../backend';
+import { RESTANGULAR_INTERCEPTORS, RestangularInterceptorHandler } from '../interceptor';
 
 @Injectable()
-export class RestangularInterceptingHandler implements RestangularHandler {
+export class RestangularInterceptingHandler implements RestangularBaseHandler {
+  private chain: RestangularBaseHandler;
   constructor(
     private backend: RestangularBackend,
     private injector: Injector
@@ -15,20 +17,24 @@ export class RestangularInterceptingHandler implements RestangularHandler {
   }
 
   handle<T>(req: RestangularRequest<T>) {
-    return this.backend.handle(req)
+    const interceptors = this.injector.get(RESTANGULAR_INTERCEPTORS, []);
+
+    this.chain = interceptors.reduceRight((next, interceptor) => new RestangularInterceptorHandler(next, interceptor), this.backend);
+
+    return this.chain.handle(req.clone({setParams: req.params, setHeaders: req.headers}))
     .pipe(
       filter(ev => ev instanceof HttpResponse)
     );
   }
 
-  withConfig(options: any): RestangularHandler {
+  withConfig(options: any): RestangularBaseHandler {
     const injector = Injector.create({
       providers: [],
     });
     return new RestangularInterceptingHandler(this.backend, injector);
   }
 
-  extendConfig(options: any): RestangularHandler {
+  extendConfig(options: any): RestangularBaseHandler {
     const injector = Injector.create({
       providers: [],
       parent: this.injector,
