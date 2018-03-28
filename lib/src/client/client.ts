bcfa24333a8b041d78cf60358432c54760a5742a
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-03-28 11:44:02 +0300

    feat(lib:client): add request methods

--- lib/src/client/client.ts
+++ lib/src/client/client.ts
@@ -1,8 +1,11 @@
+import { HttpHeaders, HttpParams } from '@angular/common/http';
+
 import { RestangularHandler } from '../handler';
 import { RestangularBuilder } from '../builder';
 import { Restangular } from '../restangular';
+import { RestangularRequest } from '../backend';
 
-export class RestangularClient implements Restangular{
+export class RestangularClient implements Restangular {
 
   constructor(
     private builder: RestangularBuilder,
@@ -27,6 +30,64 @@ export class RestangularClient implements Restangular{
   all(route: string): RestangularClient {
     const builder = this.builder.all(route);
     return new RestangularClient(builder, this.handler);
+  }
+
+  /**
+   * Method to make GET request to pointer
+   */
+  get(
+    params?: HttpParams | string | { [name: string]: string | string[] },
+    headers?: HttpHeaders | string | { [name: string]: string | string[] },
+  ) {
+    const req = new RestangularRequest('GET', this.builder.pointer, {params, headers});
+    return this.handler.handle(req);
+  }
+
+  /**
+   * Method to make POST request to pointer
+   */
+  post<T>(
+    body,
+    params?: HttpParams | string | { [name: string]: string | string[] },
+    headers?: HttpHeaders | string | { [name: string]: string | string[] },
+  ) {
+    const req = new RestangularRequest('POST', this.builder.pointer, body, {params, headers});
+    return this.handler.handle(req);
+  }
+
+  /**
+   * Method to make PUT request to pointer
+   */
+  put<T>(
+    body,
+    params?: HttpParams | string | { [name: string]: string | string[] },
+    headers?: HttpHeaders | string | { [name: string]: string | string[] },
+  ) {
+    const req = new RestangularRequest('PUT', this.builder.pointer, body, {params, headers});
+    return this.handler.handle(req);
+  }
+
+  /**
+   * Method to make PATCH request to pointer
+   */
+  patch<T>(
+    body,
+    params?: HttpParams | string | { [name: string]: string | string[] },
+    headers?: HttpHeaders | string | { [name: string]: string | string[] },
+  ) {
+    const req = new RestangularRequest('PATCH', this.builder.pointer, body, {params, headers});
+    return this.handler.handle(req);
+  }
+
+  /**
+   * Method to make DELETE request to pointer
+   */
+  delete<T>(
+    params?: HttpParams | string | { [name: string]: string | string[] },
+    headers?: HttpHeaders | string | { [name: string]: string | string[] },
+  ) {
+    const req = new RestangularRequest('DELETE', this.builder.pointer, {params, headers});
+    return this.handler.handle(req);
   }
 
   /**
