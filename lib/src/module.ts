77c2426223674543c8adec4569702f63a818f051
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-03-28 11:49:03 +0300

    feat(lib): provide default config & base interceptors

--- lib/src/module.ts
+++ lib/src/module.ts
@@ -4,12 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { RestangularHandler, RestangularInterceptingHandler } from './handler';
 import { RestangularBackend, RestangularHttpBackend } from './backend';
 import { InitialRestangular, Restangular } from './restangular';
+import { DefaultRestangularConfig, RestangularConfig } from './config';
+import { configInterceptors } from './interceptors';
 
 @NgModule({
   imports: [
     HttpClientModule
   ],
   providers: [
+    configInterceptors,
+    {provide: RestangularConfig, useValue: DefaultRestangularConfig},
     {provide: Restangular, useClass: InitialRestangular},
     {provide: RestangularHandler, useClass: RestangularInterceptingHandler},
     RestangularHttpBackend,
