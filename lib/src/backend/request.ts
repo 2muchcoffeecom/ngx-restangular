00e1b163613959ecbe5a7338265c693a600994bd
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-03-28 11:02:05 +0300

    feat(lib:backend): update Restangular Request
    update init options
    add clone method

--- lib/src/backend/request.ts
+++ lib/src/backend/request.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 
-import { toHttpHeaders, toHttpParams } from '../utils/request-utils';
+import { addToHttpHeaders, addToHttpParams, toHttpHeaders, toHttpParams } from '../utils/request-utils';
 
 function mightHaveBody(method: string): boolean {
   switch (method) {
@@ -16,11 +16,17 @@ function mightHaveBody(method: string): boolean {
 }
 
 export interface RestangularRequestOptions {
+  baseUrl?: string;
   headers?: HttpHeaders | string | { [key: string]: string | string[] };
+  reportProgress?: boolean;
+  withCredentials?: boolean;
+  responseType?: 'arraybuffer' | 'blob' | 'json' | 'text';
   params?: HttpParams | string | { [key: string]: string | string[] };
 }
 
 export class RestangularRequest<T> {
+
+  baseUrl: string;
 
   readonly body: T | null = null;
 
@@ -40,7 +46,12 @@ export class RestangularRequest<T> {
     method: 'DELETE' | 'GET' | 'HEAD' | 'JSONP' | 'OPTIONS',
     pointer: string[],
     options?: {
+      baseUrl?: string,
       headers?: HttpHeaders | string | { [key: string]: string | string[] },
+      reportProgress?: boolean,
+      withCredentials?: boolean,
+      responseType?: 'arraybuffer' | 'blob' | 'json' | 'text',
+      method?: string,
       params?: HttpParams | string | { [key: string]: string | string[] },
     },
   )
@@ -49,7 +60,11 @@ export class RestangularRequest<T> {
     pointer: string[],
     body?: T | null,
     options?: {
+      baseUrl?: string,
       headers?: HttpHeaders | string | { [key: string]: string | string[] },
+      reportProgress?: boolean,
+      withCredentials?: boolean,
+      responseType?: 'arraybuffer' | 'blob' | 'json' | 'text',
       params?: HttpParams | string | { [key: string]: string | string[] },
     },
   )
@@ -57,12 +72,21 @@ export class RestangularRequest<T> {
     method: string,
     readonly pointer: string[],
     third?: T | {
+      baseUrl?: string,
+      body?: T | null,
       headers?: HttpHeaders | string | { [key: string]: string | string[] },
+      reportProgress?: boolean,
+      withCredentials?: boolean,
+      responseType?: 'arraybuffer' | 'blob' | 'json' | 'text',
       params?: HttpParams | string | { [key: string]: string | string[] },
-
     },
     fourth?: {
+      baseUrl?: string,
+      body?: T | null,
       headers?: HttpHeaders | string | { [key: string]: string | string[] },
+      reportProgress?: boolean,
+      withCredentials?: boolean,
+      responseType?: 'arraybuffer' | 'blob' | 'json' | 'text',
       params?: HttpParams | string | { [key: string]: string | string[] },
     },
   ) {
@@ -77,13 +101,72 @@ export class RestangularRequest<T> {
       options = third as RestangularRequestOptions;
     }
 
-    this.headers = toHttpHeaders(options.headers);
+    if (options) {
+      // Normalize reportProgress and withCredentials.
+      this.reportProgress = !!options.reportProgress;
+      this.withCredentials = !!options.withCredentials;
 
-    this.params = toHttpParams(options.params);
+      // Override default response type of 'json' if one is provided.
+      if (!!options.responseType) {
+        this.responseType = options.responseType;
+      }
+
+      if (!!options.baseUrl) {
+        this.baseUrl = options.baseUrl;
+      }
+
+      this.headers = toHttpHeaders(options.headers);
+
+      this.params = toHttpParams(options.params);
+    }
+  }
+
+  clone(update: {
+    baseUrl?: string,
+    body?: T | null,
+    headers?: HttpHeaders | string | { [key: string]: string | string[] },
+    setHeaders?: HttpHeaders | { [key: string]: string | string[] },
+    reportProgress?: boolean,
+    withCredentials?: boolean,
+    responseType?: 'arraybuffer' | 'blob' | 'json' | 'text',
+    method?: string,
+    params?: HttpParams | string | { [key: string]: string | string[] },
+    setParams?: HttpParams | { [key: string]: string },
+    pointer?: string[],
+  }): RestangularRequest<T> {
+    const method = update.method || this.method;
+    const pointer = update.pointer || this.pointer;
+    const responseType = update.responseType || this.responseType;
+    const baseUrl = update.baseUrl || this.baseUrl;
+
+    const body = (update.body !== undefined) ? update.body : this.body;
+
+    const withCredentials =
+      (update.withCredentials !== undefined) ? update.withCredentials : this.withCredentials;
+    const reportProgress =
+      (update.reportProgress !== undefined) ? update.reportProgress : this.reportProgress;
+
+    let headers = (update.headers && toHttpHeaders(update.headers)) || this.headers;
+    let params = (update.params && toHttpParams(update.params)) || this.params;
+
+    if (update.setHeaders !== undefined) {
+      // Set every requested header.
+      headers = addToHttpHeaders.call(headers, update.setHeaders);
+    }
+
+    // Check whether the caller has asked to set params.
+    if (update.setParams) {
+      // Set every requested param.
+      params = addToHttpParams.call(params, update.params);
+    }
+
+    return new RestangularRequest<T>(method, pointer, body, {
+      responseType, withCredentials, reportProgress, headers, params, baseUrl
+    });
   }
 
   toHttpRequest() {
-    return new HttpRequest<T>(this.method as any, this.pointer.join('/'), this.body, {
+    return new HttpRequest<T>(this.method as any, [this.baseUrl, ...this.pointer].join('/'), this.body, {
       headers: this.headers,
       reportProgress: this.reportProgress,
       params: this.params,
