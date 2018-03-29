e23c9b4d8e5a2cdfd3f11b5a2e5fefe65eef0509
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-03-29 17:52:44 +0300

    fix(demo:mock-data): update mocking to use MockBackend instead of mock HttpClient

--- demo/mock-data/mock-providers.ts
+++ demo/mock-data/mock-providers.ts
@@ -1,22 +1,14 @@
-import {HttpClient, HttpHandler} from "@angular/common/http";
+import { HttpBackend } from "@angular/common/http";
 import {RestangularHttp} from "../../src/ngx-restangular-http";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { MockBackendService } from "./mock-backend.service";
 
 export const MockProviders = [
   MockBackendService,
   {
     provide: RestangularHttp,
-    useFactory: (http: HttpClient) => {
+    useFactory: (http: HttpBackend) => {
       return new RestangularHttp(http);
     },
-    deps: [HttpClient]
-  },
-  {
-    provide: HttpClient,
-    useFactory: (backendInstance: MockBackendService) => {
-      return new HttpClient(backendInstance);
-    },
     deps: [MockBackendService]
-  },
+  }
 ];
