796a8e8509a1866ac31a390d7df7e6b2434aaf85
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-04-04 19:20:57 +0300

    build(config): add externals file to import in all configs

---
+++ config/externals.js
@@ -0,0 +1,12 @@
+export default [
+  'lodash',
+  '@angular/core',
+  '@angular/common/http',
+  'core-js/fn/object',
+  'rxjs/BehaviorSubject',
+  'rxjs/Observable',
+  'rxjs/add/observable/throw',
+  'rxjs/add/operator/filter',
+  'rxjs/add/operator/map',
+  'rxjs/add/operator/catch',
+]
