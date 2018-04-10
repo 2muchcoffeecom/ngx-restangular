2fcd58d191dc50a7f3051763de5a6bf3075d7375
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-04-10 11:13:00 +0300

    feat(lib:handler): add default headers setting & refactor url forming

--- lib/src/handler/intercepting-handler.ts
+++ lib/src/handler/intercepting-handler.ts
@@ -1,11 +1,12 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpBackend, HttpRequest, HttpResponse } from '@angular/common/http';
+import { HttpBackend, HttpHeaders, HttpRequest, HttpResponse } from '@angular/common/http';
 
 import { filter } from 'rxjs/operators/filter';
 
 import { RestangularBaseHandler } from './handler';
 import { RestangularRequest } from '../backend';
 import { RestangularConfig } from '../config';
+import { appendHeaders, escapeSlash, isHttpHeaders, normalizeUrl } from '../utils';
 
 @Injectable()
 export class RestangularInterceptingHandler implements RestangularBaseHandler {
@@ -18,10 +19,12 @@ export class RestangularInterceptingHandler implements RestangularBaseHandler {
   ) {
   }
 
-  handle<T>({method, builder, body, params, headers}: RestangularRequest<T>) {
+  handle<T>({method, builder, body, params, headers = new HttpHeaders()}: RestangularRequest<T>) {
     this.injectConfig();
 
     const url = this.getNormalizedUrl(builder.pointer);
+
+    headers = this.extendHeadersWithDefault(headers);
 
     const httpRequest = new HttpRequest(
       method,
@@ -62,24 +65,24 @@ export class RestangularInterceptingHandler implements RestangularBaseHandler {
   private getUrl(pointer: string[]) {
     const baseUrl = typeof this.config.baseUrl === 'undefined' ? '' : this.config.baseUrl;
 
-    return pointer.reduce((acc, item) => this.escapeSlash(acc) + '/' + item, baseUrl);
+    return pointer.reduce((acc, item) => escapeSlash(acc) + '/' + item, baseUrl);
   }
 
   private getNormalizedUrl(pointer: string[]) {
-    return this.normalizeUrl(this.getUrl(pointer));
+    return normalizeUrl(this.getUrl(pointer));
   }
 
-  private normalizeUrl(url: string) {
-    const result = /((?:http[s]?:)?\/\/)?(.*)?/.exec(url);
-    const schemeWithSlash = result[1];
-    const restPart = result[2].replace(/[\\\/]+/g, '/');
-    return (typeof schemeWithSlash !== 'undefined') ? schemeWithSlash + restPart : restPart;
-  }
-
-  private escapeSlash(url: string = '') {
-    if (typeof url === 'string') {
-      return url.replace(/\/$/, '');
+  private extendHeadersWithDefault(headers: HttpHeaders) {
+    const defaultHeaders = this.config.defaultHeaders;
+    if (headers && !isHttpHeaders(headers)) {
+      throw new Error('headers must be instance of HttpHeaders');
     }
-    throw new Error('Couldn\'t transform not string values');
+    if (defaultHeaders && !isHttpHeaders(defaultHeaders)) {
+      throw new Error('Configuration defaultHeaders must be instance of HttpHeaders');
+    }
+    if (defaultHeaders && isHttpHeaders(defaultHeaders)) {
+      return appendHeaders(defaultHeaders, headers, this.config.appendHeaders);
+    }
+    return headers || new HttpHeaders();
   }
 }
