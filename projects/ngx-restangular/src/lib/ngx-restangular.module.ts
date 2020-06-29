2484dd9f439f393e62546bd60514a57968b493a7
Robin <Robin.Steltmann@googlemail.com>
2020-06-29 13:39:41 +0200

    Update to Angular 10

--- projects/ngx-restangular/src/lib/ngx-restangular.module.ts
+++ projects/ngx-restangular/src/lib/ngx-restangular.module.ts
@@ -19,9 +19,9 @@ export class RestangularModule {
     }
   }
 
-  static forRoot(configFunction?: (provider: any, ...arg: any[]) => void): ModuleWithProviders;
-  static forRoot(providers?: any[], configFunction?: (provider: any, ...arg: any[]) => void): ModuleWithProviders;
-  static forRoot(config1?, config2?): ModuleWithProviders {
+  static forRoot(configFunction?: (provider: any, ...arg: any[]) => void): ModuleWithProviders<any>;
+  static forRoot(providers?: any[], configFunction?: (provider: any, ...arg: any[]) => void): ModuleWithProviders<any>;
+  static forRoot(config1?, config2?): ModuleWithProviders<any> {
     return {
       ngModule: RestangularModule,
       providers: [
